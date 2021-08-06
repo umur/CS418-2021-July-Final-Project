f8f58bec2d87853098e2c9d43580e004bcd3a86b
Amanuel Abuhay <aaamare36@gamil.com>
2021-08-06 13:03:39 -0500

    test

--- Product_Rating_App/routes/users.js
+++ Product_Rating_App/routes/users.js
@@ -10,4 +10,8 @@ router.get("/:id", function (req, res, next) {
   res.send("respond with a resource for id");
 });
 
+router.get("/rating", function (req, res, next) {
+  res.send("respond with a resource for id");
+});
+
 module.exports = router;
