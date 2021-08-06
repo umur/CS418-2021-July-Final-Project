8bac956a0d08b5c75bc9b6a8c6d495894305faaf
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-08-06 16:27:00 -0700

    setup mongoDb connection

--- product-review-app/app.js
+++ product-review-app/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
 
 
 var indexRouter = require('./routes/index');
+// var testRouter = require('./test');
 
 
 var app = express();
@@ -16,8 +18,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+let connect
+
+app.use((req,res,next)=>{
+  if(!connect){
+    console.log("connected...")
+    MongoClient.connect('mongodb+srv://Asgedom:Asgedom@cluster0.dg7cy.mongodb.net/shopDB?retryWrites=true&w=majority', { useUnifiedTopology: true })
+         .then(client => {
+            connect = client.db('shopDB');
+          req.db = connect;
+          next();
+        })
+        .catch(err => console.log('Error: ', err));
+  }else{
+    req.db = connect
+    next();
+  }
+})
+
+app.get('/test',(req,res)=>{
+  req.db.collection('item').findOne()
+  .then(data=>{
+    res.json(data)
+    console.log(data);
+  })
+})
+
 
 app.use('/', indexRouter);
+// app.use('/test', testRouter);
 
 
 // error handler
