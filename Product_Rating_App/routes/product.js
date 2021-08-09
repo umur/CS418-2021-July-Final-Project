a91a941dfc19584ff0d8184e803ac79b3221c60a
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-08-09 14:06:51 -0400

    productIncluded

--- Product_Rating_App/routes/product.js
+++ Product_Rating_App/routes/product.js
@@ -3,49 +3,58 @@ const router = express.Router();
 const authConroller = require("../controller/authController");
 let getDB = require("../utils/database").getDB;
 
-
 router.get("/", (req, res, next) => {
-
   let product1 = getDB().collection("product").find().toArray();
   product1.then((data) => {
-    if(!data){console.log("no data")}
-    res.json({data});
+    if (!data) {
+      console.log("no data");
+    }
+    res.json({ data });
   });
 
   //res.json("ready to get");
 });
 router.get("/:name", (req, res, next) => {
   //res.json("ready to get with name");
-  let product1 = getDB().collection("product").find({"productName":req.params.name}).toArray();
+  let product1 = getDB()
+    .collection("product")
+    .find({ productName: req.params.name })
+    .toArray();
   product1.then((data) => {
-    if(!data){console.log("no data")}
-    res.json({data});
+    if (!data) {
+      console.log("no data");
+    }
+    res.json({ data });
   });
-
 });
 router.post("/", (req, res, next) => {
   let product1 = getDB().collection("product").insertMany([req.body]);
   product1.then((data) => {
-    if(!data){console.log("no data")}
-    res.json({data});
+    if (!data) {
+      console.log("no data");
+    }
+    res.json({ data });
   });
-
 });
-router.put("/:name", authConroller.authorizeAdmin,
- (req, res, next) => {
-  let product = getDB().collection("product").updateOne({"productName":req.params.name},{$set:req.body})
-  product.then((data)=>{
-    res.json(data)
-  })
-
+router.put("/:name", authConroller.authorizeAdmin, (req, res, next) => {
+  let product = getDB()
+    .collection("product")
+    .updateOne({ productName: req.params.name }, { $set: req.body });
+  product.then((data) => {
+    res.json(data);
+  });
 });
 router.delete("/:name", authConroller.authorizeAdmin, (req, res, next) => {
- 
-  let productname = req.params.name
-  let product = getDB().collection("product").deleteOne({"productName":productname})
-  product.then((data)=>{if(!productname){console.log("no product name")}
-res.json({data})})
+  let productname = req.params.name;
+  let product = getDB()
+    .collection("product")
+    .deleteOne({ productName: productname });
+  product.then((data) => {
+    if (!productname) {
+      console.log("no product name");
+    }
+    res.json({ data });
+  });
 });
 
 module.exports = router;
-
