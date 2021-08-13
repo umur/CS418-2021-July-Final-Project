25d365ab026ee15c1141145bc0369f90a8ad4625
Tam Nguyen <elainetmo1511@gmail.com>
2021-08-13 13:02:58 -0500

    Update products.js

--- PRODUCT REVIEW PROJECT/BACKEND/routes/products.js
+++ PRODUCT REVIEW PROJECT/BACKEND/routes/products.js
@@ -2,25 +2,18 @@ const express = require('express');
 const router = express.Router();
 const productController=require('../controllers/productController');
 const userController=require('../controllers/userController');
-// const ObjectId=require('mongodb').ObjectId;
 
 
-//fetch all the products
 router.get('/',productController.getAllProducts);
 
-//get product by Sku
 router.get('/:prodSku',productController.getProdBySku);
 
-// add products
 router.post('/',productController.addProducts);
 
-//update product-authorize superUser
 router.put('/:prodSku',userController.authorizedSuperUser, productController.updateProdBySku)
 
-//delete product-authorize superUser
 router.delete('/:prodSku', userController.authorizedSuperUser, productController.deleteProdBySku)
 
-//add review for prodSku
 router.post('/review',productController.addReview);
 
 
